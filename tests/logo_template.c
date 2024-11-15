b1d5ddd0d28d2471302aedfeeebd1176d245adb8
Rafał Jopek <rafaljopek@hotmail.com>
2024-11-15 19:06:18 +0100

    Update logo_template.c

--- tests/logo_template.c
+++ tests/logo_template.c
@@ -8,9 +8,9 @@ int main()
 {
    int width = 512, height = 512;
 
-   SVG *svg = svg_init( "hc.svg", width, height );
+   SVG *svg = svg_init( "logo_template.svg", width, height );
 
-   svg_filled_circle( svg, width / 2, height / 2, 256, 0x13A10E );
+   svg_filled_circle( svg, width / 2, height / 2, 256, 0x530972 );
 
    svg_line( svg, 42, ( height - 250 ) / 2, width - 42, ( height - 250 ) / 2, 30, 0x323232 );
 
@@ -20,7 +20,7 @@ int main()
 
    svg_circle( svg, width / 2, height / 2, 256 - 15, 30, 0x323232 );
 
-   svg_text( svg, ( width - 150 ) / 2, ( height + 20 ) / 2, "HC", "Free Mono", 120, FONT_WEIGHT_BOLD, 0x323232 );
+   svg_text( svg, ( width - 3 * ( 140 * 0.6 ) ) / 2, ( height + 20 ) / 2, "C++", "Free Mono", 140, FONT_WEIGHT_BLACK, 0x323232 );
 
    svg_close( svg );
 
